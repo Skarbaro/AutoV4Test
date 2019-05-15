b21f1d07ad82a180bb2e3021f09687be2c891a1c
Alex Romaniuk <alaster.rom@gmail.com>
2019-05-15 22:35:55 +0300

    test 1 LoginNew

--- src/test/java/login/LogInWithOutPageObject.java
+++ src/test/java/login/LogInWithOutPageObject.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.io.File;
 import java.util.concurrent.TimeUnit;
 
-public class Login {
+public class LogInWithOutPageObject {
     WebDriver webDriver;
 
     @Before
