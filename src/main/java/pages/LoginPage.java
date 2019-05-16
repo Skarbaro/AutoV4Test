05d4d8be0703711071a8a4e17015b5343c498457
Alex Romaniuk <alaster.rom@gmail.com>
2019-05-16 20:30:20 +0300

    test 2 ActionsWithOurElements

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -3,8 +3,18 @@ package pages;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class LoginPage extends ParentPage {
+    @FindBy(name = "_username")
+    WebElement inputLogin;
+
+    @FindBy(id = "password")
+    WebElement inputPassWord;
+
+    @FindBy(tagName = "button")
+    WebElement buttonVhod;
 
     public LoginPage(WebDriver webDriver) {
         super(webDriver);
@@ -21,35 +31,30 @@ public class LoginPage extends ParentPage {
     }
 
     public void enterLogin(String login) {
-        try {
-            webDriver.findElement(By.name("_username")).clear();
-            webDriver.findElement(By.name("_username")).sendKeys(login);
-            logger.info(login + " was inputted into Input");
-        } catch (Exception e) {
-            logger.error("Cannot work with element.");
-            Assert.fail("Cannot work with element.");
-        }
+        actionsWithOurElements.enterTextInToElement(inputLogin, login);
+//        try {
+//            inputPassWord.clear();
+//            inputPassWord.sendKeys(passWord);
+//            logger.info(passWord + " was inputted into Input");
+//        } catch (Exception e) {
+//            logger.error("Cannot work with element.");
+//            Assert.fail("Cannot work with element.");
+//        }
     }
 
     public void enterPassWord(String passWord) {
-        try {
-            webDriver.findElement(By.id("password")).clear();
-            webDriver.findElement(By.id("password")).sendKeys(passWord);
-            logger.info(passWord + " was inputted into Input");
-        } catch (Exception e) {
-            logger.error("Cannot work with element.");
-            Assert.fail("Cannot work with element.");
-        }
+        actionsWithOurElements.enterTextInToElement(inputPassWord, passWord);
     }
 
     public void clickButtonVhod() {
-        try {
-            webDriver.findElement(By.tagName("button")).click();
-            logger.info("Click Button.");
-        } catch (Exception e) {
-            logger.error("Cannot work with element.");
-            Assert.fail("Cannot work with element.");
-        }
+        actionsWithOurElements.clickOnElement(buttonVhod);
+//        try {
+//            buttonVhod.click();
+//            logger.info("Click Button.");
+//        } catch (Exception e) {
+//            logger.error("Cannot work with element.");
+//            Assert.fail("Cannot work with element.");
+//        }
     }
 
 
--- src/test/java/login/LogInNew.java
+++ src/test/java/login/LogInNew.java
@@ -2,6 +2,7 @@ package login;
 
 import org.junit.Assert;
 import org.junit.Test;
+import pages.LoginPage;
 import parenTest.ParenTest;
 
 public class LogInNew extends ParenTest {
@@ -12,6 +13,16 @@ public class LogInNew extends ParenTest {
         loginPage.enterPassWord("909090");
         loginPage.clickButtonVhod();
 
-        Assert.assertTrue("Avatar is not present", homePage.isAvatarPresent());
+        Assert.assertTrue("Avatar is not present", homePage.isAvatarPresent());  // valid
+    }
+
+    @Test
+    public void noValidLogin(){
+        loginPage.OpenLoginPage();
+        loginPage.enterLogin("Students");
+        loginPage.enterPassWord("9090090");
+        loginPage.clickButtonVhod();
+
+        Assert.assertFalse("Avatar is not present", homePage.isAvatarPresent()); // not valid
     }
 }
