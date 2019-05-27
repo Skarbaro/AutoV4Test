0c8eef2876e8bf62326ec98a39d0ea6a84496afc
Mike <mihail.xan@yandex.ua>
2019-05-27 12:30:57 +0300

    Commit SparePage 1.1

---
+++ src/main/java/pages/EditSparePage.java
@@ -0,0 +1,20 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class EditSparePage extends ParentPage{
+    @FindBy(id = "spares_spareName")
+    private WebElement spareNameInput;
+
+    @FindBy(id = "spares_spareType")
+    private WebElement spareTypeDD;
+
+    @FindBy(name = "delete")
+    private WebElement buttonDelete;
+
+    public EditSparePage(WebDriver webDriver){
+        super (webDriver, "/dictionary/spares/edit");
+    }
+}
--- src/main/java/pages/SparePage.java
+++ src/main/java/pages/SparePage.java
@@ -1,11 +1,26 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
 
 public class SparePage extends ParentPage {
+    @FindBy (xpath = "//table//tr")
+    List<WebElement> listOfRows;
 
     public SparePage(WebDriver webDriver) {
         super(webDriver, "/dictionary/spares");
     }
 
+    public void checkAndDeleteSpare(String spareName) {
+        for (WebElement line: listOfRows) {
+            WebElement cellWithSpare = line.findElement(By.xpath("//td[1]"));
+            if (cellWithSpare.getText().equals(spareName)) {
+                actionsWithOurElements.clickOnElement(cellWithSpare);
+            }
+        }
+    }
 }
