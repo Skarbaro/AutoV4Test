12eeec508114f80eeeec688dffd47c1e71777fd1
Alex Romaniuk <alaster.rom@gmail.com>
2019-06-06 23:56:18 +0300

    test checkbox 1.4 testExcelData

--- src/test/java/login/LogInNewWithDataFromExcel.java
+++ src/test/java/login/LogInNewWithDataFromExcel.java
@@ -1,7 +1,6 @@
 package login;
 
 import libs.ExcelDriver;
-import org.junit.Assert;
 import org.junit.Test;
 import parenTest.ParentTest;
 
@@ -14,13 +13,12 @@ public class LogInNewWithDataFromExcel extends ParentTest {
         ExcelDriver excelDriver = new ExcelDriver();
         Map dataForValidLogin = excelDriver.getData(configProperties.DATA_FILE(),"validLogOn");
 
-
         loginPage.openLoginPage();
         loginPage.enterLogin(dataForValidLogin.get("login").toString());
         loginPage.enterPassWord(dataForValidLogin.get("pass").toString());
         loginPage.clickButtonVhod();
 
-        checkExpectedResult("Avatar is not present",homePage.isAvatarDisplayed());
+        checkExpectedResult("Avatar is not present", homePage.isAvatarDisplayed());
 //        Assert.assertTrue("Avatar is not present", homePage.isAvatarDisplayed());  // valid
     }
 
