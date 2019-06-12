ab89f681e495e3ca3b59853f0df7c2dc3e5a30d2
Alex Romaniuk <alaster.rom@gmail.com>
2019-06-12 23:29:01 +0300

    1.0 restTestPB Excel

---
+++ src/main/java/restSteps/ParentApi.java
@@ -0,0 +1,8 @@
+package restSteps;
+
+import org.aeonbits.owner.ConfigFactory;
+import resources.ApiEndPoints;
+
+public class ParentApi {
+    protected static ApiEndPoints apiEndPoints = ConfigFactory.create(ApiEndPoints.class);
+}
